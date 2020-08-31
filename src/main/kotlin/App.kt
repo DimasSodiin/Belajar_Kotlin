3d8d3d1ea354188d54122f0f69cb8f85f8945451
Dimasodiin <dimassodiin7777@gmail.com>
2020-08-31 13:35:29 +0700

    first commit

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,10 @@
+/*
+Nama: Dimas Sodiin
+Kelas: XI RPL 2
+Absen: 09
+ */
+fun main () {
+    println("==============================")
+    println("=========HELLO KOTLIN=========")
+    println("==============================")
+}
